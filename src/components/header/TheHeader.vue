95fa8340a5d7108facb8cf5099045417d9514506
olajoao <kusterzito@gmail.com>
2022-05-23 19:11:03 -0300

    feat: header created

---
+++ src/components/header/TheHeader.vue
@@ -0,0 +1,76 @@
+<template>
+  <header class="bg-purple-700 text-white flex">
+    <!-- left -->
+    <nav class="flex-1 flex items-center justify-between p-3">
+      <div class="flex gap-5">
+        <div>
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect opacity="0.2" width="50" height="50" rx="5" fill="white" />
+            <path
+              opacity="0.2"
+              d="M14.2917 38.625H34.7083C35.4819 38.625 36.2237 38.3177 36.7707 37.7707C37.3177 37.2237 37.625 36.4819 37.625 35.7083V15.2917C37.625 14.5181 37.3177 13.7763 36.7707 13.2293C36.2237 12.6823 35.4819 12.375 34.7083 12.375H14.2917C13.5181 12.375 12.7763 12.6823 12.2293 13.2293C11.6823 13.7763 11.375 14.5181 11.375 15.2917V35.7083C11.375 36.4819 11.6823 37.2237 12.2293 37.7707C12.7763 38.3177 13.5181 38.625 14.2917 38.625ZM18.6667 28.4167L22.1127 31.8627L27.4167 24.0417L34.7083 34.25H14.2917L18.6667 28.4167Z"
+              fill="#E0E6EA"
+            />
+          </svg>
+        </div>
+        <div class="w-60 border border-white rounded-md text-sm py-1 px-2">
+          <p class="font-bold">Surreal São Paulo</p>
+          <span class="opacity-70">VTEX</span>
+        </div>
+      </div>
+      <div>
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16 4V6.76923M8.24246 16.9231C8.24246 12.3077 9.21216 6.76923 16 6.76923C22.7879 6.76923 23.7576 12.3077 23.7576 16.9231C23.7576 21.5385 26.6667 24.3077 26.6667 24.3077H5.33337C5.33337 24.3077 8.24246 21.5385 8.24246 16.9231ZM19.8788 24.3077C19.8788 24.3077 19.8788 28 16 28C12.1213 28 12.1213 24.3077 12.1213 24.3077H19.8788Z"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </nav>
+
+    <!-- right -->
+    <aside
+      class="w-60 border-l-2 border-white/20 flex justify-center items-center gap-3"
+    >
+      <p class="font-bold">Olá, Joaquim</p>
+      <button class="cursor-pointer">
+        <svg
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            class="opacity-40 hover:opacity-60"
+            cx="27"
+            cy="27"
+            r="27"
+            fill="currentColor"
+          />
+          <path
+            d="M27.5 18C24.8808 18 22.75 20.1249 22.75 22.7368C22.75 25.3487 24.8808 27.4737 27.5 27.4737C30.1191 27.4737 32.25 25.3487 32.25 22.7368C32.25 20.1249 30.1191 18 27.5 18ZM27.5 25.5789C25.9287 25.5789 24.65 24.3038 24.65 22.7368C24.65 21.1699 25.9287 19.8947 27.5 19.8947C29.0713 19.8947 30.35 21.1699 30.35 22.7368C30.35 24.3038 29.0713 25.5789 27.5 25.5789ZM36.05 36V35.0526C36.05 31.3967 33.066 28.4211 29.4 28.4211H25.6C21.933 28.4211 18.95 31.3967 18.95 35.0526V36H20.85V35.0526C20.85 32.4407 22.9808 30.3158 25.6 30.3158H29.4C32.0191 30.3158 34.15 32.4407 34.15 35.0526V36H36.05Z"
+            fill="white"
+          />
+        </svg>
+      </button>
+    </aside>
+  </header>
+</template>
+
+<script></script>
