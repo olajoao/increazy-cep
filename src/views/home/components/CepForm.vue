02ef67e76d4aa59ddda429013d213be4ff6a7990
olajoao <kusterzito@gmail.com>
2022-05-23 23:26:47 -0300

    feat: default button hover

--- src/views/home/components/CepForm.vue
+++ src/views/home/components/CepForm.vue
@@ -4,7 +4,7 @@
       <label for="cep">
         <input
           v-model="cep"
-          class="p-2 outline-none ring-2 focus:ring-zinc-500 rounded-md border border-zinc-300 placeholder:text-zinc-400 text-zinc-600 font-medium"
+          class="p-2 outline-none ring-2 focus:ring-purple-primary/60 rounded-md border border-zinc-300 placeholder:text-zinc-400 text-zinc-600 font-medium"
           :class="[isCepWrong ? 'ring-red-500' : 'ring-transparent']"
           type="number"
           placeholder="Insira o CEP"
@@ -45,3 +45,17 @@ function changeCep() {
 
 watchEffect(() => changeCep());
 </script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
